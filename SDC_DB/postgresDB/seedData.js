9afba114e064d82ff3a8106cb1a7c509e19f055c
Makan Azarshahy <oro1919@gmail.com>
2018-03-20 11:54:57 -0700

    Added seed data generator script

--- SDC_DB/postgresDB/seedData.js
+++ SDC_DB/postgresDB/seedData.js
@@ -1,27 +1,81 @@
-const pg = require('pg');
 const fs = require('fs');
-const { exec } = require('child_process');
-const itemGen = require('./menuItemGen');
+const { generateMenuItem } = require('./menuItemGen');
 const nameGen = require('./restaurantNameGen');
+const tagGen = require('./menuTagGen');
 
-const nameStream = fs.createWriteStream('./testData/names.csv');
-const itemStream = fs.createWriteStream('./testData/items.csv');
+const nameStream = fs.createWriteStream(`${__dirname}/testData/names.csv`);
+const itemStream = fs.createWriteStream(`${__dirname}/testData/items.csv`);
+const tagStream = fs.createWriteStream(`${__dirname}/testData/tags.csv`);
 
-const write10MItems = (start = 1e7) => {
-  let i = start;
+const write480MTags = (start = 48e7) => {
+  let index = start;
   let freeSpace = true;
 
-  while (i > 0 && freeSpace) {
-    const item = itemGen(i);
-    freeSpace = itemStream.write(item);
-    i -= 1;
+  while (index > 0 && freeSpace) {
+    const tag = tagGen(index);
+    freeSpace = tagStream.write(tag);
+    index -= 1;
   }
 
-  if (i > 0) {
-    itemStream.once('drain', () => {
-      write10MItems(i);
-    });
+  if (index > 0) {
+    tagStream.once('drain', () => write480MTags(index));
   }
 };
 
-write10MItems();
+
+const write10MNames = (start = 1e7) => {
+  let index = start;
+  let freeSpace = true;
+  while (index > 0 && freeSpace) {
+    const name = nameGen(index);
+    freeSpace = nameStream.write(name);
+    index -= 1;
+  }
+
+  if (index > 0) {
+    nameStream.once('drain', () => write10MNames(index));
+  }
+
+  if (index === 0) {
+    write480MTags();
+  }
+};
+
+const generateMenu = (resId) => {
+  const menu = [];
+  for (let i = 0; i < 48; i += 1) {
+    let item;
+    if (i < 20) {
+      item = generateMenuItem('LUNCH', resId);
+    } else if (i < 40) {
+      item = generateMenuItem('DINNER', resId);
+    } else {
+      item = generateMenuItem('DESSERT', resId);
+    }
+    menu.push(item);
+  }
+  return menu;
+};
+
+const write480MItems = (start = 1e7) => {
+  let index = start;
+  let freeSpace = true;
+
+  while (index > 0 && freeSpace) {
+    const menu = generateMenu(index);
+    freeSpace = itemStream.write(`${menu.join('\n')}\n`);
+    index -= 1;
+  }
+
+  if (index > 0) {
+    itemStream.once('drain', () => {
+      write480MItems(index);
+    });
+  }
+
+  if (index === 0) {
+    write10MNames();
+  }
+};
+
+write480MItems();
