62dfee2eda19c23a526b61d0c939aeeece8b0bca
Makan Azarshahy <oro1919@gmail.com>
2018-03-28 12:19:34 -0700

    Refactored code to use redis and mongo EC2 url

--- src/nodeServer/server.js
+++ src/nodeServer/server.js
@@ -4,9 +4,8 @@ const app = require('./app');
 const redisClient = require('./redisClient');
 
 const port = process.env.PORT || 3005;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/silverspoon';
+const MONGO_URI = 'mongodb://ec2-52-53-248-96.us-west-1.compute.amazonaws.com/silverspoon';
 
 mongoose.connect(MONGO_URI, { poolSize: 10 });
-
 
 app.listen(port, () => console.log(`LISTENING ---> ${port}`));
